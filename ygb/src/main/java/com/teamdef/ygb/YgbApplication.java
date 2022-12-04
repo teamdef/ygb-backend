a6a5a44d0dad1741f8f0428fc3eda4ba251219fe
dev_qorh <qorhkdgh00@gmail.com>
2022-12-04 11:18:17 +0900

    init

---
+++ ygb/src/main/java/com/teamdef/ygb/YgbApplication.java
@@ -0,0 +1,19 @@
+package com.teamdef.ygb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class YgbApplication {
+	@RequestMapping("/")
+	public String home() {
+		return "Hello Docker World";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(YgbApplication.class, args);
+	}
+
+}
---
+++ ygb/src/test/java/com/teamdef/ygb/YgbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamdef.ygb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YgbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
